60250f1b608daa0460d322dc187753f97e0995b0
FabianSchuetze <fabian.schutze@eui.eu>
2019-07-05 15:10:34 +0100

    Has nan policy as well

--- dataframe/ConstColumnIterator.h
+++ dataframe/ConstColumnIterator.h
@@ -1,8 +1,8 @@
 #ifndef GUARD_ConstColumnIterator_h
 #define GUARD_ConstColumnIterator_h
+#include <cmath>
 #include <iostream>
 #include "dataframe.h"
-#include <cmath>
 
 class Column;
 template <class T>
@@ -14,7 +14,8 @@ class DataFrame::ConstColumnIterator {
     friend bool operator!=(const const_iter<V>&, const const_iter<V>&);
     ConstColumnIterator(const DataFrame& a, int n, size_t sz = 0)
         : theDataFrame(a), wptr(a.columns[n]), curr(sz), iteration_order() {
-        iteration_order = a.index.find_index_position();}
+        iteration_order = a.index.find_index_position();
+    }
     std::string to_string();
     const T& operator*() const;
     const T& operator[](int) const;
@@ -55,8 +56,7 @@ DataFrame::ConstColumnIterator<T> DataFrame::cbegin(const std::string& s) {
 template <class T>
 DataFrame::ConstColumnIterator<T> DataFrame::cend(const std::string& s) {
     try {
-        return ConstColumnIterator<T>(*this, column_names.at(s),
-                                      index.size());
+        return ConstColumnIterator<T>(*this, column_names.at(s), index.size());
     } catch (std::out_of_range& e) {
         throw std::out_of_range("Column: " + s + " not found");
     }
@@ -120,121 +120,89 @@ DataFrame::const_iter<T> DataFrame::ConstColumnIterator<T>::operator--(int) {
     return ret;
 }
 
-template <class T>
-typename std::vector<std::pair<int, T>>::iterator partition_pairs(
-    std::vector<std::pair<int, T>>& inp) {
-    if constexpr (std::is_same<T, double>::value) {
-        auto fun = [&](std::pair<int, T>& a) { return !std::isnan(a.second); };
-        return std::stable_partition(inp.begin(), inp.end(), fun);
-    } else if constexpr (std::is_same<T, std::string>::value) {
-        auto fun = [&](std::pair<int, T>& a) { return !(a.second == "NA"); };
-        return std::stable_partition(inp.begin(), inp.end(), fun);
-    }
-}
-
 template <typename E>
 void group_equal_values(std::vector<std::tuple<int, E, int>>& inp) {
     int group_id = 0;
     std::get<2>(inp[0]) = group_id;
     for (size_t i = 1; i < inp.size(); i++) {
-        if (std::get<1>(inp[i]) != std::get<1>(inp[i-1])) group_id++;
+        if (std::get<1>(inp[i]) != std::get<1>(inp[i - 1])) group_id++;
         std::get<2>(inp[i]) = group_id;
     }
 }
 
 template <typename N, typename E>
-std::vector<std::tuple<int, N, int>> replace_val(DataFrame::const_iter<N>& b,
-        std::vector<std::tuple<int, E,int>>& inp) {
+std::vector<std::tuple<int, N, int>> replace_val(
+    DataFrame::const_iter<N>& b, std::vector<std::tuple<int, E, int>>& inp) {
     std::vector<std::tuple<int, N, int>> new_index(inp.size());
     for (size_t i = 0; i < inp.size(); i++) {
         int curr = std::get<0>(inp[i]);
-        std::cout << b[curr] << std::endl;
         new_index[i] = std::make_tuple(curr, b[curr], std::get<2>(inp[i]));
     }
     return new_index;
 }
 
 template <typename T>
+bool is_nan(const T& t) {
+    if constexpr (std::is_same<T, double>::value)
+        return std::isnan(t);
+    else if constexpr (std::is_same<T, std::string>::value)
+        return t == "NA";
+}
+
+template <typename T>
 void sort_pairs(std::vector<std::tuple<int, T, int>>& inp) {
     auto fun = [&](auto& a, auto& b) -> bool {
-        if (std::get<2>(a) == std::get<2>(b))
-            return std::get<1>(a) < std::get<1>(b);
-        else
+        if (std::get<2>(a) == std::get<2>(b)) {
+            if (is_nan(std::get<1>(a)))
+                return true;  // make sort non-stable
+            else
+                return std::get<1>(a) < std::get<1>(b);
+        } else
             return false;
     };
     std::sort(inp.begin(), inp.end(), fun);
 }
 
 template <typename E>
-std::vector<int> 
-return_indices(const std::vector<std::tuple<int, E, int>>& inp) {
+std::vector<int> return_indices(
+    const std::vector<std::tuple<int, E, int>>& inp) {
     std::vector<int> res(inp.size());
-    for (size_t i = 0; i < inp.size(); i++)
-        res[i] = std::get<0>(inp[i]);
+    for (size_t i = 0; i < inp.size(); i++) res[i] = std::get<0>(inp[i]);
     return res;
 }
 
 template <typename E, typename none = void>
 std::vector<int> sort(std::vector<std::tuple<int, E, int>> inp) {
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
     sort_pairs<E>(inp);
     return return_indices<E>(inp);
 }
 
 template <typename E, typename T1, typename... T2>
-std::vector<int> 
-sort(DataFrame::const_iter<T1> v1, DataFrame::const_iter<T2>... v2,
-     std::vector<std::tuple<int, E, int>>& inp){
-    std::cout << "\n";
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
+std::vector<int> sort(DataFrame::const_iter<T1> v1,
+                      DataFrame::const_iter<T2>... v2,
+                      std::vector<std::tuple<int, E, int>>& inp) {
     sort_pairs<E>(inp);
     group_equal_values(inp);
-    for (auto const v : inp)
-        std::cout << std::get<0>(v) << " " << std::get<1>(v) << " " <<
-            std::get<2>(v) << ",  ";
-    std::cout << std::endl;
     std::vector<std::tuple<int, T1, int>> res = replace_val<T1, E>(v1, inp);
-    for (auto const v : res)
-        std::cout << std::get<0>(v) << " " << std::get<1>(v) << " " <<
-            std::get<2>(v) << ",  ";
-    std::cout << std::endl;
     return sort(v2..., res);
 }
 
-template <class T>
-std::vector<int> get_arguments(const std::vector<std::pair<int, T>>& inp) {
-    std::vector<int> res(inp.size(), 0);
-    std::transform(inp.begin(), inp.end(), res.begin(),
-                   [](const std::pair<int, T>& ele) { return ele.first; });
-    return res;
-}
-
 template <typename T1, typename... T2>
-std::vector<int> 
-permutation_index(DataFrame::const_iter<T1>& it,
-                  DataFrame::const_iter<T2>&... it2, DataFrame& df) {
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
-    std::vector<std::tuple<int, T1, int>> res(df.size().first);
-    for (size_t i = 0; i < res.size(); ++i) 
-        res[i] = std::make_tuple(i, *it++, 0);
-    for (auto const v : res)
-        std::cout << std::get<0>(v) << " " << std::get<1>(v) << " " <<
-            std::get<2>(v) << std::endl;
-    //typename std::vector<std::pair<int, T>>::iterator boundary;
-    //boundary = partition_pairs<T>(res);
+std::vector<int> permutation_idx(DataFrame::const_iter<T1>& it,
+                                 DataFrame::const_iter<T2>&... it2,
+                                 size_t len) {
+    std::vector<std::tuple<int, T1, int>> res(len);
+    for (size_t i = 0; i < len; ++i) res[i] = std::make_tuple(i, *it++, 0);
     return sort<T1, T2...>(it2..., res);
 }
 
 template <typename T1, typename... T2>
-void DataFrame::sort_by_column(ConstColumnIterator<T1>v1,
-                               ConstColumnIterator<T2>...v2) {
+void DataFrame::sort_by_column(ConstColumnIterator<T1> v,
+                               ConstColumnIterator<T2>... v2) {
     std::vector<std::deque<Index::ele>> new_index;
-    std::vector<int> argsort = 
-        permutation_index<T1, T2...>(v1, v2..., *this);
-    for (size_t i = 0; i < argsort.size(); ++i) {
-        auto e = index.index_positions[argsort[i]];
-        new_index.push_back(e);
-    }
+    std::vector<int> idx = permutation_idx<T1, T2...>(v, v2..., size().first);
+    for (size_t i = 0; i < idx.size(); ++i)
+        new_index.push_back(index.index_positions[idx[i]]);
     index.index_positions = new_index;
 }
 #endif
